cc1f62d58d2d5932856e961306d30801976688c2
marcelfa <marsel.fadel@gmail.com>
2024-01-18 19:17:43 +0200

    adding category interface

--- types/Plants.tsx
+++ types/Plants.tsx
@@ -4,24 +4,37 @@ interface Plants{
 }
 
 interface Plant {
-    id: number;
+    id: string;
     name: string;
-    imageId: string;
-    seedToCrop: string;
-    yield: string;
-    lifeCycle: string;
-    description: string;
-    nutritionFacts: NutritionFacts;
+    imageId?: string;
+    seedToCrop?: string;
+    yield?: string;
+    lifeCycle?: string;
+    description?: string;
+    nutritionFacts?: NutritionFacts;
 
   }
 
   interface NutritionFacts{
-    portionInfo: string;
-    items: [ItemAttributes];
+    portionInfo?: string;
+    items?: [ItemAttributes];
   }
 
   interface ItemAttributes{
-    key: string;
-    nutrientValue: string;
-    percentageOfRDA: string;
+    key?: string;
+    nutrientValue?: string;
+    percentageOfRDA?: string;
   }
+
+  interface Category{
+    id: string;
+    name: string;
+    minSelection?: number;
+    maxSelection?: number;
+    plants: [Plant]
+  }
+
+  interface Categories{
+
+    categories: [Category];
+}
